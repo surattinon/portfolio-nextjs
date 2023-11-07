741c9ea12deca7d48329dab2be0f58959ff93f9c
surattinon <surattinon.basu@gmail.com>
2023-11-08 00:52:16 +0700

    change order when screen lg in section 2

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -41,7 +41,7 @@ const HeroSection = () => {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-12 sm:flex-wrap-reverse container py-8 bg-white mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 flex-wrap container py-8 bg-white mb-8">
         <div className="col-span-7 place-self-center body-font lg:text-center text-left">
           <div className = 'mx-auto'>
             <button className = 'w-[200px] h-[60px] bg-black p-2 text-white hover:shadow-[0_20px_50px_rgba(100,_100,_100,_0.7)] hover:shadow-[0px_10px_1px_rgba(121,_121,_121,_1),_0_10px_20px_rgba(158,_158,_158,_1)] hover:-translate-y-3 duration-300'>
@@ -51,7 +51,7 @@ const HeroSection = () => {
             </button>
           </div>
         </div>
-        <div className="col-span-5 place-self-center">
+        <div className="col-span-5 place-self-center order-first lg:order-last">
           <div className="lg:width-[200px] md:width-[100px] py-8 ">
             <Image
               src="/images/profile-img.jpeg"
--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'BASU INFO',
+  title: 'BASU > INFO',
   description: 'This is my info website.',
 }
 
