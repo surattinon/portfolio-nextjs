064b0b5ca6314f998dd52ea75dd6b1284f470e02
SurattinonH <basu@mac-basu.local>
2023-11-08 00:13:44 +0700

    add margin of section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="h-[600px] col-span-5">
+      <div className="grid grid-cols-1 lg:grid-cols-12 my-10">
+        <div className="col-span-5 ">
           <div className="place-self-center lg:width-[800px] md:width-[300px]">
             <Image
               src="/images/web-logo-header.png"
@@ -41,7 +41,7 @@ const HeroSection = () => {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-12 sm:flex-wrap-reverse container py-8 bg-white">
+      <div className="grid grid-cols-1 lg:grid-cols-12 sm:flex-wrap-reverse container py-8 bg-white mb-8">
         <div className="col-span-7 place-self-center body-font lg:text-center text-left">
           <div className = 'mx-auto'>
             <button className = 'w-[200px] h-[60px] bg-black p-2 text-white hover:shadow-[0_20px_50px_rgba(100,_100,_100,_0.7)] hover:shadow-[0px_10px_1px_rgba(121,_121,_121,_1),_0_10px_20px_rgba(158,_158,_158,_1)] hover:-translate-y-3 duration-300'>
