af42ab34f9c0c8369ad27938f5e6235cd42cf2ab
バース <surattinon.basu@gmail.com>
2024-03-23 16:48:35 +0700

    commit

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,7 +1,7 @@
-"use client"
-import { TypeAnimation } from 'react-type-animation';
-import React from "react";
-import Image from "next/image";
+'use client'
+import { TypeAnimation } from 'react-type-animation'
+import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
@@ -22,12 +22,7 @@ const HeroSection = () => {
         <div className=" col-span-7 place-self-center body-font lg:text-center text-left">
           <h1 className="text-white mb-4 text-4xl md:text-6xl font-light ">
             <TypeAnimation
-              sequence={[
-                '<INFO />',
-                2000, 
-                '<Surattinon />',
-                2000
-              ]}
+              sequence={['<INFO />', 2000, '<Surattinon />', 2000]}
               wrapper="span"
               speed={30}
               repeat={Infinity}
@@ -36,18 +31,14 @@ const HeroSection = () => {
           <p className="text-[#b3b3b3]">
             Hello, my name is Bas!! This is my first Portfolio with NEXTJS
           </p>
-          <p>
-            So how can I make this nextjs look good?
-          </p>
+          <p>So how can I make this nextjs look good?</p>
         </div>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-12 flex-wrap container py-8 bg-white mb-8">
         <div className="col-span-7 place-self-center body-font lg:text-center text-left">
-          <div className = 'mx-auto'>
-            <button className = 'w-[200px] h-[60px] bg-black p-2 text-white hover:shadow-[0_20px_50px_rgba(100,_100,_100,_0.7)] hover:shadow-[0px_10px_1px_rgba(121,_121,_121,_1),_0_10px_20px_rgba(158,_158,_158,_1)] hover:-translate-y-3 duration-300'>
-                <h1 className = 'text-lg font-bold'>
-                  Download CV
-                </h1>
+          <div className="mx-auto">
+            <button className="w-[200px] h-[60px] bg-black p-2 text-white hover:shadow-[0_20px_50px_rgba(100,_100,_100,_0.7)] hover:shadow-[0px_10px_1px_rgba(121,_121,_121,_1),_0_10px_20px_rgba(158,_158,_158,_1)] hover:-translate-y-3 duration-300">
+              <h1 className="text-lg font-bold">Download CV</h1>
             </button>
           </div>
         </div>
@@ -58,13 +49,13 @@ const HeroSection = () => {
               alt="basu logo"
               width={200}
               height={200}
-              className = 'rounded-full'
+              className="rounded-full"
             />
           </div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default HeroSection;
+export default HeroSection
--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -2,8 +2,14 @@ import Link from 'next/link'
 
 const NavLink = ({ href, title, target }) => {
   return (
-    <Link href = { href } target = { target } className = "block py-2 pl-3 pr-4 text-gray-700 sm:text-xl md:p-0 hover:text-black">{title}</Link>
-  ) 
+    <Link
+      href={href}
+      target={target}
+      className="block py-2 pl-3 pr-4 text-gray-700 sm:text-xl md:p-0 hover:text-black"
+    >
+      {title}
+    </Link>
+  )
 }
 
-export default NavLink;
+export default NavLink
